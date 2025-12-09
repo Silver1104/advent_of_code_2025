87e1bfd3ce9babf322ea8fd895dc8f00cda0a235
Raghav Poddar <raghavpoddar39@gmail.com>
2025-12-09 22:37:45 +0530

    Day 8 both parts

---
+++ day8/part2.cpp
@@ -0,0 +1,148 @@
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <stack>
+#include <algorithm>
+#include <unordered_map>
+
+typedef long long ll;
+
+class disJointSet {
+private:
+    std::vector<int> par;
+    std::vector<int> size;
+    std::map<int,int> mpp;
+public:
+    disJointSet(const int n) {
+        par.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++) {
+            par[i] = i;
+            mpp[1]++;
+        }
+    }
+    int findPar(int x) {
+        if (par[x] == x) return x;
+        return findPar(par[x]);
+    }
+    void unionize(const int &u, const int &v) {
+        int pu = findPar(u);
+        int pv = findPar(v);
+        if (pu == pv) return;
+        if (size[pu] > size[pv]) {
+            par[pv] = pu;
+            if (--mpp[size[pu]] == 0) mpp.erase(size[pu]);
+            if (--mpp[size[pv]] == 0) mpp.erase(size[pv]);
+            size[pu] += size[pv];
+            mpp[size[pu]]++;
+        }
+        else {
+            par[pu] = pv;
+            if (--mpp[size[pu]] == 0) mpp.erase(size[pu]);
+            if (--mpp[size[pv]] == 0) mpp.erase(size[pv]);
+            size[pv] += size[pu];
+            mpp[size[pv]]++;
+        }
+    }
+    std::map<int,int> getAllSizes() {
+        return mpp;
+    }
+    void printSet() {
+        for (auto& el : mpp) {
+            std::cout << "For size " << el.first << ", count: " << el.second << std::endl;
+        }
+    }
+    int getTopMap() {
+        auto it = mpp.begin();
+        return it->first;
+    }
+};
+
+std::pair<int,int> makeAllGroups(const std::map<double, std::vector<std::pair<int, int>>> dist, const int &n) {
+    disJointSet ds(n);
+    int amt{};
+    bool end = false;
+    while (true) {
+        for (auto& curr_dist : dist) {
+            for (auto& coord : curr_dist.second) {
+                ds.unionize(coord.first, coord.second);
+                if (ds.getTopMap() == 1000) return { coord.first, coord.second };
+            }
+        }
+    }
+}
+
+double findDist(const std::vector<int> &coord1, const std::vector<int> &coord2) {
+    double sqx = static_cast<double>(pow(coord1[0] - coord2[0], 2));
+    double sqy = static_cast<double>(pow(coord1[1] - coord2[1], 2));
+    double sqz = static_cast<double>(pow(coord1[2] - coord2[2], 2));
+    double tot = sqx + sqy + sqz;
+    double dist = static_cast<double>(pow(tot, 0.5));
+    return dist;
+}
+
+std::map<double,std::vector<std::pair<int,int>>> makeDistMap(const std::vector<std::vector<int>> coords, const int &n) {
+    std::map<double, std::vector<std::pair<int, int>>> mpp;
+    for (int i = 0; i < n-1; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            double currdist = findDist(coords[i], coords[j]);
+            mpp[currdist].push_back({ i,j });
+        }
+    }
+    return mpp;
+}
+
+ll runOnce() {
+    std::ifstream infile("day8.txt");
+    if (!infile.is_open()) {
+        std::cerr << "Failed to open file." << std::endl;
+        return 1;
+    }
+
+    std::vector<std::vector<int>> coords;
+    std::string line;
+    int n{};
+    while (std::getline(infile, line)) {
+        size_t first = line.find(',');
+        size_t second = line.find(',', first + 1);
+        int x = stoi(line.substr(0, first));
+        int y = stoi(line.substr(first + 1, second - first - 1));
+        int z = stoi(line.substr(second + 1));
+        coords.push_back({ x,y,z });
+        n++;
+    }
+    infile.close();
+    std::map<double, std::vector<std::pair<int, int>>> distances = makeDistMap(coords, n);
+    std::pair<int,int> last = makeAllGroups(distances, n);
+    ll res = 1LL * coords[last.first][0] * 1LL * coords[last.second][0];
+    return res;
+}
+
+int main() {
+    using namespace std::chrono;
+
+    const int RUNS = 5;
+    double totalTime = 0;
+
+    for (int i = 1; i <= 5; i++) {
+
+        auto start = high_resolution_clock::now();
+
+        ll result = runOnce();   // <-- CALL YOUR COMPUTATION
+
+        auto end = high_resolution_clock::now();
+        double ms = duration_cast<milliseconds>(end - start).count();
+
+        std::cout << "Run " << i << " time: " << ms << " ms, result=" << result << std::endl;
+
+        totalTime += ms;
+    }
+
+    std::cout << "\nAverage time = " << (totalTime / RUNS) << " ms\n";
+    return 0;
+}
+
